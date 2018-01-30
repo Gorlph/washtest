4d1fe8a8d4ef433b9c1320d206daefe769582c19
Thanathip <gorlph@hotmail.com>
2018-01-30 20:15:42 +0700

    first

---
+++ code.js
@@ -0,0 +1,32 @@
+const axios =require('axios')
+const URLs=[
+  "http://api.box24corp.com/rest/index.php/test/Ex/one",
+  "http://api.box24corp.com/rest/index.php/test/Ex/two",
+  "http://api.box24corp.com/rest/index.php/test/Ex/three",
+  "http://api.box24corp.com/rest/index.php/test/Ex/four",
+  "http://api.box24corp.com/rest/index.php/test/Ex/five"]
+
+  const makeRequest = async ()=>{
+    let result=''
+    for(let i = 0; i<URLs.length;i++){
+      try{
+      let res = await axios(URLs[i])
+      // console.log(res.data)
+      let status = res.data.status
+      // console.log(status)
+      let data = res.data.data
+      let append =(status=="Suscess")?true:false
+      if(append){
+        result = result+data
+        if(i!=URLs.length-1)result = result+"*"
+      }
+    }
+    catch(e){
+
+    }
+
+    }
+    console.log(result)
+
+  }
+makeRequest()
